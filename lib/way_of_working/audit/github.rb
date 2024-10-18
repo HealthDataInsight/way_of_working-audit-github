8db0d50c3f9e154f691d611147cd0c3c348ac301
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-18 12:39:15 +0100

    More code

--- lib/way_of_working/audit/github.rb
+++ lib/way_of_working/audit/github.rb
@@ -15,20 +15,17 @@ module WayOfWorking
   end
 
   module SubCommands
-    # # This reopens the "way_of_working audit" sub command
-    # class Audit
-    #   register(Audit::Github::Generators::Audit, 'audit', 'audit',
-    # end
-    
-    # # This reopens the "way_of_working document" sub command
-    # class Document
-    #   register(Audit::Github::Generators::Document, 'audit', 'audit',
-    # end
-    
-    # # This reopens the "way_of_working exec" sub command
-    # class Exec
-    #   register(Audit::Github::Generators::Exec, 'audit', 'audit',
-    # end
+    # This reopens the "way_of_working exec" sub command
+    class Exec
+      register(Audit::Github::Generators::Exec, 'audit_github', 'audit_github',
+               <<~LONGDESC)
+                 Description:
+                     This runs the GitHub audit
+
+                 Example:
+                     way_of_working exec audit_github
+               LONGDESC
+    end
 
     # # This reopens the "way_of_working init" sub command
     # class Init
